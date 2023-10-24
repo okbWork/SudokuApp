cd38142d8985b7c029b7662b023153bc97db66db
okbWork <okb3@njit.edu>
2023-10-23 22:01:05 -0400

    Added a basic library to generate sudoku puzzles along with their solution. Uses code from https://medium.com/@typical.dev/lets-make-the-sudoku-generator-library-in-kotlin-8e0dd45c72b6 which takes a very brute force approach to generating sudoku puzzles with single solutions. Includes other file SudokuPlayerModel.kt which would hold a players info along with record info for the sake of things like leaderboards.

---
+++ app/src/main/java/com/example/sudokuapp/SudokuPlayerModel.kt
@@ -0,0 +1,12 @@
+package com.example.sudokuapp
+
+import java.util.Date
+
+data class SudokuPlayerModel(
+    var userEmail:String? = null,
+    var userId: String? = null,
+    var minTime: Int? = null,
+    val minMoves: Int? = null,
+    var highScore: Int? = null,
+    var dailyStreak: Int? = null,
+    var lastDay: Date? = null)
